1043ea7aad6ffa6b92cd8cbe32704e595a327065
nouhaila <nohaila.chkhonty@gmail.com>
2024-11-06 13:40:46 +0100

    first commit

---
+++ src/main/java/org/cn/fplarachesmartlibuser/FplaracheSmartlibUserApplication.java
@@ -0,0 +1,13 @@
+package org.cn.fplarachesmartlibuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplaracheSmartlibUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FplaracheSmartlibUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cn/fplarachesmartlibuser/FplaracheSmartlibUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cn.fplarachesmartlibuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplaracheSmartlibUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
